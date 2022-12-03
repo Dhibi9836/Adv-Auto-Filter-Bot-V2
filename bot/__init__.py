da23f274cddf77b080395d4ada52e51c78a11473
Dhibi9836 <107266932+Dhibi9836@users.noreply.github.com>
2022-12-03 21:31:05 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,7 +17,7 @@ API_HASH = "5e51ecf5945605c711fffe7b376fa2a8"
 
 BOT_TOKEN = "5694714864:AAHBWW5y2eZ-6xyGuygZOxhLWV5CxaA40HU"
 
-DB_URI = "mongodb+srv://123:123@cluster0.jypxi61.mongodb.net/test"
+DB_URI = "mongodb+srv://123:123@cluster1.7wqy6xv.mongodb.net/?retryWrites=true&w=majority"
 
 USER_SESSION = "BQB_nNsE1PEkULOcrwVt--IBeVF0rMdWQJ-_mnf1DiJ8CI1TSmxS98HVj6ypLTG1brVbpvYRrkYwWMAEEHa-l8hIiQNFpwSgJ2Zt-5mODv8BcwR6bhvWiXAk_8cl14R8ST21lGYKPSVqYZGYBBME8MZ59i2GqJc7dpB76wUR2KrcuYp6sM-C-8A0mo1I7EsMLM8dHXhkM-amjbL-BWFD_5JAvD59TuChPOuzP8qWFELR2SpASfgiPDynaj58VnkUyLNnLBfjqjj8QAsHry8RpvhizcnGqxDPB-NhhZ6qMXZ0U6a9cadp3UrzeuYKLY2itQZLrFvG4C6MMGPFWSd3zyXdRcYZbgA"
 
